e73f164f55a634f4583e83be894417de06defacc
Raffaj1208 <61484778+Raffaj1208@users.noreply.github.com>
2020-05-13 17:11:34 -0700

    ..

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -1,14 +1,14 @@
 //..
-let animals = ["lion", "dog", "ape", "bear"];
+let newAnimals = ["lion", "dog", "gorilla", "bear"];
 
 //..
 function addButton(){
     $("#btnView").empty();
-    for (let i = 0; i < animals.length; i++){
+    for (let i = 0; i < newAnimals.length; i++){
         let a = $("<button>");
         a.addClass("animal");
-        a.attr("data-name", animals[i]);
-        a.text(animals[i]);
+        a.attr("data-name", newAnimals[i]);
+        a.text(newAnimals[i]);
         $("#btnView").append(a);
     }
 }
@@ -16,9 +16,15 @@ function addButton(){
 //..
 $("#submit").on("click", function(event){
     event.preventDefault();
-    let animal = $("#input").val().trim();
-    animals.push(animal);
+    let newAnimal = $("#input").val().trim();
+    newAnimals.push(newAnimal);
     addButton();
 });
 //..
 addButton();
+
+//.. Create a function that runs when the button is clicked 
+$("#btnView").on("click", function(){
+    let gifDisplay = $(this).attr("btnView");
+    let queryURL = "" + gifDisplay + "&api_key=zUw0bnZlqKeg8r03F07nOOSOcXkjgXnN";
+});
