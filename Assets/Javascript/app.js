6a51a91e6b0a2627aa2269673db1db29c193fc7a
Michael Pope <michaelpopedeveloper@gmail.com>
2020-05-16 13:48:10 -0700

    michals update

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -1,54 +1,65 @@
 //..
 let newAnimals = ["Lion", "Tiger", "Gorilla", "Polar Bear"];
+
 //..
-function addButton(){
-    $("#btnView").empty();
-    $("#contentView").empty();
-    for (let i = 0; i < newAnimals.length; i++){
-        let a = $("<button>");
-        a.addClass("animal");
-        a.attr("data-animal", newAnimals[i]);
-        a.text(newAnimals[i]);
-        $("#btnView").append(a);
-    }
+function addButton() {
+  $("#btnView").empty();
+  $("#contentView").empty();
+  for (let i = 0; i < newAnimals.length; i++) {
+    let a = $("<button>");
+    a.addClass("animal");
+    a.attr("data-animal", newAnimals[i]); // changed from data-name, bacause on line 31 you get get the attribute
+    a.text(newAnimals[i]);
+    $("#btnView").append(a);
+  }
+  onClick(); // added onClick, because you need to set the onClick handlers again after adding a new button
 }
+
 //..
-$("#submit").on("click", function(event){
-    event.preventDefault();
-    let newAnimal = $("#input").val().trim();
-    newAnimals.push(newAnimal);
-    addButton();
+$("#submit").on("click", function (event) {
+  event.preventDefault();
+  let newAnimal = $("#input").val().trim();
+  newAnimals.push(newAnimal);
+  addButton();
 });
 //..
 addButton();
-
 //..
-function onClick(){
-$("#btnView").on("click", function(){
+function onClick() {
+  $(".animal").on("click", function () { // changed from #btnView, because you want to do this when you click on the button, not the div
     $("#contentView").empty();
+    let animal = $(this).attr("data-animal"); 
+    console.log("Animal: ", this);
+    let queryURL =
+      "https://api.giphy.com/v1/gifs/search?q=" +
+      animal +
+      "&api_key=zUw0bnZlqKeg8r03F07nOOSOcXkjgXnN&limit=5";
 
-let animal = $(this).data("data-animal");
-let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=zUw0bnZlqKeg8r03F07nOOSOcXkjgXnN&limit=5";
-//..
-$.ajax({
-    url: queryURL,
-    method: "GET"
-})
-//..
-.then(function(response){
-    console.log(queryURL);
-    console.log(response);
-    let results = response.data;
-    for (i = 0; i < results.length; i++){
-        let animalDiv = $("<div>");
-        let p = $("<p>").text("Rating:" + results[i].rating);
-        let animalImage = $("<img>");
-        animalImage.attr("src", results[i].images.fixed_height.url);
-        animalDiv.append(p);
-        animalDiv.append(animalImage);
-        $("#contentView").prepend(animalDiv);
+    //..
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    })
+
+      //..
+      .then(function (response) {
+        console.log(queryURL);
+        console.log(response);
+        let results = response.data;
+        for (i = 0; i < results.length; i++) {
+          let animalDiv = $("<div>");
+          let p = $("<p>").text("Rating:" + results[i].rating);
+          let animalImage = $("<img>");
+          animalImage.attr("src", results[i].images.fixed_height.url);
+          animalDiv.append(p);
+          animalDiv.append(animalImage);
+          $("#contentView").prepend(animalDiv);
         }
-    });
+      });
   });
 }
 onClick();
+//..
+function clear() {
+  $("#contentView").empty();
+}
